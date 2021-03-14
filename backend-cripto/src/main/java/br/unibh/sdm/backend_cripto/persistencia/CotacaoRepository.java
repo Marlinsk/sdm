36d7ca6cd31b274f29cdaed2393bb6b0ab32bd0b
Jose Humberto Cruvinel <jhcruvinel@yahoo.com.br>
2021-03-13 21:46:12 -0300

    Projeto SpringBoot com acesso à AWS DynamoDB

---
+++ backend-cripto/src/main/java/br/unibh/sdm/backend_cripto/persistencia/CotacaoRepository.java
@@ -0,0 +1,16 @@
+package br.unibh.sdm.backend_cripto.persistencia;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import br.unibh.sdm.backend_cripto.entidades.Cotacao;
+
+@EnableScan()
+public interface CotacaoRepository extends CrudRepository<Cotacao, UUID> {
+	
+	List<Cotacao> findByCodigo(String codigo);
+	
+}
---
+++ backend-cripto/src/main/java/br/unibh/sdm/backend_cripto/persistencia/CriptomoedaRepository.java
@@ -0,0 +1,16 @@
+package br.unibh.sdm.backend_cripto.persistencia;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import br.unibh.sdm.backend_cripto.entidades.Criptomoeda;
+
+@EnableScan()
+public interface CriptomoedaRepository extends CrudRepository<Criptomoeda, UUID> {
+	
+	List<Criptomoeda> findByNome(String nome);
+	
+}
